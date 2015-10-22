9d4e39d258bafe98769b3aef2a50777a4369424d
Bernhard <temeise@t-online.de>
2015-10-22 16:46:51 +0200

    initial recommit of the project

---
+++ WedoIT/other_resources/SupplierManagementTool/app/js/app.js
@@ -0,0 +1,15 @@
+var app = angular.module('SupplierManagement', [
+    'ngRoute',
+    'smart-table'
+]);
+
+/*app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider.
+    when("/supplier", {
+        templateUrl: "partials/supplier.html",
+        controller: "OneSupplierCtrl"
+    })
+    otherwise({
+        redirectTo: '/index'
+    });
+}]);*/
