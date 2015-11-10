c9c249623051756e7e0180c73093732b26bb1eeb
Joncn89 <jonas.ringler@gmx.de>
2015-11-10 16:09:20 +0100

    JavaDoc for AllDao added

--- src/main/java/edu/hm/wedoit/dao/AllDao.java
+++ src/main/java/edu/hm/wedoit/dao/AllDao.java
@@ -10,9 +10,23 @@ import java.util.List;
  */
 public interface AllDao
 {
+    /**
+     * Returns a list of all suppliers
+     * @return The list of the suppliers
+     */
     public List<Supplier> getAllSuppliersWithScore();
 
+    /**
+     * Returns a list of orders for one supplier
+     * @param id the LIFNR of a supplier
+     * @return the list of orders for the supplier or null if the suppliers-id doesn't exist
+     */
     public List<Order> getAllOrdersForId(String id);
 
+    /**
+     * Returns the supplier with the given LIFNR
+     * @param id the LIFNR of the supplier
+     * @return the supplier with the given id or null if this supplier doesn't exist
+     */
     public Supplier getSupplierById(String id);
 }
