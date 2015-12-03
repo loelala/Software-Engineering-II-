064a800abafe1d0c2b20875d2b89cb89fcc30ec0
Joncn89 <jonas.ringler@gmx.de>
2015-12-03 10:33:47 +0100

    ScoringLimits refactored to non-static

--- src/main/java/edu/hm/wedoit/model/limits/ScoringLimits.java
+++ src/main/java/edu/hm/wedoit/model/limits/ScoringLimits.java
@@ -8,7 +8,7 @@ import java.util.Map;
  */
 public class ScoringLimits
 {
-    public enum limits
+    public enum Limits
     {
         PMIN100,
         PMAX100,
@@ -44,25 +44,25 @@ public class ScoringLimits
         scoringLimits = limits;
     }
 
-    public static Map<String, Integer> getDefaultMap()
+    public Map<String, Integer> getDefaultMap()
     {
         Map<String, Integer> map = new HashMap<>();
-        map.put(limits.PMIN100.toString(), 0);
-        map.put(limits.PMAX100.toString(), 0);
-        map.put(limits.NMIN100.toString(), -1);
-        map.put(limits.NMAX100.toString(), -1);
+        map.put(Limits.PMIN100.toString(), 0);
+        map.put(Limits.PMAX100.toString(), 0);
+        map.put(Limits.NMIN100.toString(), -1);
+        map.put(Limits.NMAX100.toString(), -1);
 
-        map.put(limits.PMAX90.toString(), 3);
-        map.put(limits.NMIN90.toString(), -2);
+        map.put(Limits.PMAX90.toString(), 3);
+        map.put(Limits.NMIN90.toString(), -2);
 
-        map.put(limits.PMAX80.toString(), 7);
-        map.put(limits.NMIN80.toString(), -3);
+        map.put(Limits.PMAX80.toString(), 7);
+        map.put(Limits.NMIN80.toString(), -3);
 
-        map.put(limits.PMAX60.toString(), 14);
-        map.put(limits.NMIN60.toString(), -7);
+        map.put(Limits.PMAX60.toString(), 14);
+        map.put(Limits.NMIN60.toString(), -7);
 
-        map.put(limits.PMAX40.toString(), 28);
-        map.put(limits.NMIN40.toString(), -10);
+        map.put(Limits.PMAX40.toString(), 28);
+        map.put(Limits.NMIN40.toString(), -10);
 
         return map;
     }
