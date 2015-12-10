0c90fff5663147339c15fd035d328f52756e0e3f
Joncn89 <jonas.ringler@gmx.de>
2015-12-10 09:08:02 +0100

    Added validation for given limits

--- src/main/java/edu/hm/wedoit/rest/SettingsController.java
+++ src/main/java/edu/hm/wedoit/rest/SettingsController.java
@@ -3,13 +3,14 @@ package edu.hm.wedoit.rest;
 import edu.hm.wedoit.model.limits.ClassificationLimits;
 import edu.hm.wedoit.model.limits.ScoringLimits;
 import edu.hm.wedoit.settingsmanagement.SettingsManagement;
+import edu.hm.wedoit.utils.ClassificationUtils;
+import edu.hm.wedoit.utils.ScoringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Joncn on 10.12.2015.
@@ -23,6 +24,9 @@ public class SettingsController
     @Autowired
     SettingsManagement sm;
 
+    private ScoringUtils su = ScoringUtils.getInstance();
+    private ClassificationUtils cu = ClassificationUtils.getInstance();
+
     @RequestMapping("/classification")
     public ClassificationLimits getClassificationLimits()
     {
@@ -35,5 +39,39 @@ public class SettingsController
     {
         logger.debug("getScoringLimits()");
         return sm.getScoringLimits();
+    }
+
+    @RequestMapping(value = "/scoring",method = RequestMethod.POST)
+    public ResponseEntity<String> setScoringLimits(@RequestBody ScoringLimits newLimits)
+    {
+        logger.debug("setScoringLimits({})",newLimits);
+        if (su.validateScoringLimits(newLimits))
+        {
+            sm.setScoringlimits(newLimits);
+            ResponseEntity<String> responseEntity = new ResponseEntity<String>(HttpStatus.OK);
+            return responseEntity;
+        }
+        else
+        {
+            ResponseEntity<String> responseEntity = new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+            return responseEntity;
+        }
+    }
+
+    @RequestMapping(value = "/classification",method = RequestMethod.POST)
+    public ResponseEntity<String> setClassificationLimits(@RequestBody ClassificationLimits newLimits)
+    {
+        logger.debug("setClassificationLimits({})",newLimits);
+        if (cu.validateClassificationLimits(newLimits))
+        {
+            sm.setClassificationLimits(newLimits);
+            ResponseEntity<String> responseEntity = new ResponseEntity<String>(HttpStatus.OK);
+            return responseEntity;
+        }
+        else
+        {
+            ResponseEntity<String> responseEntity = new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+            return responseEntity;
+        }
     }
 }
